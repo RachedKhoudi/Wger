927f6d853b5227716fe04d883c1d4e344df1ed49
rachad khoudi <khoudirachad@gmail.com>
2021-11-13 13:35:47 +0100

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wger
+//
+//  Created by Rached Khoudi on 13/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
